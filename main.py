9247ecb92aae23b28411507fc0e859abcb720744
Cleyrol Santana <cleyrolsc@gmail.com>
2024-08-31 02:30:49 -0400

    Add functionality to keep for script running if no company is found

--- main.py
+++ main.py
@@ -13,20 +13,27 @@ async def main():
    
 
     with open("institution_data.csv", mode="w", newline="") as institution_csv:
-        fieldnames = ["rnc", "nombre_comercial", "regimen_pago", "estado", "actividad_economica"]
+        # Added field name ("No encontrado") for the times in which the company is not found
+        fieldnames = ["rnc", "nombre_comercial", "regimen_pago", "estado", "actividad_economica", "no_encontrado"]
 
         csv_writer = csv.DictWriter(institution_csv, fieldnames=fieldnames)
         csv_writer.writeheader()
 
         for rnc in rnc_list:
             await browser_navigation.look_up_rnc(rnc)
+            # Added if-else statement to handle different element when company does not show up, also created different function
             try:
-                await browser_navigation.first_page.waitForSelector("#ctl00_cphMain_dvDatosContribuyentes > tbody > tr:nth-child(3) > td:nth-child(2)")
-                extracted_data = await browser_navigation.extract_data(rnc)
-                csv_writer.writerow(extracted_data)
+                if await browser_navigation.first_page.querySelector("#ctl00_cphMain_dvDatosContribuyentes > tbody > tr:nth-child(3) > td:nth-child(2)"):
+                    extracted_data = await browser_navigation.extract_data(rnc)
+                    csv_writer.writerow(extracted_data)
+                else:
+                    await browser_navigation.first_page.querySelector("#ctl00_cphMain_lblInformacion")
+                    extracted_data2 = await browser_navigation.extract_data2(rnc)
+                    csv_writer.writerow(extracted_data2)
             except errors.TimeoutError as error:
                 continue
 
 asyncio.get_event_loop().run_until_complete(main())
 
-# if __name__ = 
+# if __name__ == "__main__":
+#     asyncio.run(main())
